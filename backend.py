4a398c3c4d52370a8892ab153809a0b858beeb3d
KokoseiJ <kokoseij@gmail.com>
2020-01-11 01:27:08 +0900

    get_texture 메소드 삭제, 텍스쳐 로드 시 배경이 검정색으로 로딩되는 오류 수정

--- backend.py
+++ backend.py
@@ -8,6 +8,10 @@ import json
 
 # TODO: 예외 클래스 따로 생성해둘것
 
+#####################
+##### Functions #####
+#####################
+
 def resize(surf, size):
     # 각각 길이에 size인수 값 곱해서 스케일 후 리턴
     return pygame.transform.scale(surf, tuple(map(lambda x: int(x*size), surf.get_size())))
@@ -25,37 +29,46 @@ def get_texture(image, tile_size = (32, 32), texture_size = (512, 512)):
         # 타일 위치 지정하는 Rect 생성
         # 시작 지점 = (x % 가로타일개수 * 타일 길이, x / 가로타일개수 * 타일 길이)
         srcrect = pygame.Rect(int(x % width) * tile_size[0], int(x / width) * tile_size[1], tile_size[0], tile_size[1])
-        surf = pygame.Surface(tile_size).convert_alpha()
-        surf.blit(image, (0, 0), srcrect)
-        rtnlist.append(surf)
+        rtnlist.append(image.subsurface(srcrect))
     return tuple(rtnlist)
+
+#####################
+###### Classes ######
+#####################
 
 class rpg_map:
     def __init__(self, name):
+        ### 데이터 로딩 ###
         self.texture = get_texture(pygame.image.load(os.path.join("maps", name, "texture.png")).convert_alpha())
         # TODO: BMP 파싱 도무지 못해먹겠으니 알아서 구현할것. 임시로 텍스트 파싱으로 가져다둠
         self.floor = json.load(open(os.path.join("maps", name, "floor")))
         self.object = json.load(open(os.path.join("maps", name, "object")))
-
-    def get_surf(self):
+        ### 맵 Surface 생성 ###
         # floor 파일에서 맵 사이즈 구해오기. 단, 맵은 직사각형이어야 함
         height = len(self.floor)
         width = len(self.floor[0])
         # Surface 생성 후 타일별로 그려서 리턴
-        rtnsurf = pygame.Surface((width * 32, height * 32)).convert_alpha()
+        self.mapsurf = pygame.Surface((width * 32, height * 32), flags = pygame.SRCALPHA)
         for y in range(height):
             for x in range(width):
-                texturenumb = int.from_bytes(bytearray.fromhex(self.floor[y][x][:2]), sys.byteorder)
-                rtnsurf.blit(self.texture[texturenumb], (x * 32, y * 32))
-        return rtnsurf
+                # 색상코드 앞 2자리 받아서 바이트로 변환후 int로 재변환
+                floornumb = int.from_bytes(bytearray.fromhex(self.floor[y][x][:2]), sys.byteorder)
+                objectnumb = int.from_bytes(bytearray.fromhex(self.object[y][x][:2]), sys.byteorder)
+                self.mapsurf.blit(self.texture[floornumb], (x * 32, y * 32))
+                self.mapsurf.blit(self.texture[objectnumb], (x * 32, y * 32))
+
+        ### 맵 충돌 포인트 생성 ###
+        self.hitbox = tuple(map(lambda x:  map(lambda y:  int(y[2:4]), x), self.object))
+
+    #def get_surf(self, )
 
 class character:
     def __init__(self, name):
         self.texture = get_texture(pygame.image.load(os.path.join("characters", name + ".png")).convert_alpha(), texture_size = (96, 128))
-    def get_surf(self):
-        rtnsurf = pygame.Surface((96, 128)).convert_alpha()
+    def texture_test(self):
+        rtnsurf = pygame.Surface((96, 128), flags = pygame.SRCALPHA)
         for y in range(4):
             for x in range(3):
                 texturenumb = y * 3 + x
                 rtnsurf.blit(self.texture[texturenumb], (x * 32, y * 32))
-        return rtnsurf
+        return rtnsurf 
--- test.py
+++ test.py
@@ -6,11 +6,12 @@ from backend import *
 pygame.init()
 size = (1280, 720)
 screen = pygame.display.set_mode(size)
-screen.fill((0, 0, 0))
+#screen.fill((0, 0, 0))
+screen.fill((55, 55, 55))
 pygame.display.set_caption('IMC RPG')
 clock = pygame.time.Clock()
 
-test = rpg_map("test_doyun")
+test = rpg_map("test")
 testchr = character("test")
 
 while True:
@@ -19,4 +20,4 @@ while True:
             exit()
     mapsurf = resize(testchr.get_surf(), 3)
     screen.blit(mapsurf, (0, 0))
-    pygame.display.update()
+    pygame.display.flip()
